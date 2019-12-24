cead94dadce789b0d65aef4c740a797b161e3284
carr-jar <1773298425@qq.com>
2019-12-24 08:53:33 +0800

    修改

--- src/main/java/com/zyp/cms/utils/StringUtils.java
+++ src/main/java/com/zyp/cms/utils/StringUtils.java
@@ -102,6 +102,18 @@ public class StringUtils {
 		return find;
 	}
 	/**
+	 * 判断是否有数字
+	 */
+	public static boolean isHaveNumber(String str) {
+		char[] charArray = str.toCharArray();
+		for (char c : charArray) {
+			if(c>='0' && c<='9') {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
 	 * 判断手机号是否为数值，是否长度为11位，开始位必须是1.
 	 */
 	public static boolean judgeTelephoneIsOk(String src){
@@ -112,4 +124,50 @@ public class StringUtils {
 		return find;
 		
 	}
+	/**
+	 * 验证是否是URL
+	 * @param url
+	 * @return
+	 */
+	public static boolean isHttpUrl(String str){
+		
+		 //转换为小写
+        str = str.toLowerCase();
+        String regex = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@  
+               + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184               
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "[0-9a-z]*"  // 或单域名
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.  
+                 + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名  
+                + "[a-z]{2,6})" // first level domain- .com or .museum  
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name  
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
+        return  str.matches(regex);	
+	}
+	/**
+          * 地球半径,单位 km
+     */
+    private static final double EARTH_RADIUS = 6378.137;
+    public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
+        // 纬度
+        double lat1 = Math.toRadians(latitude1);
+        double lat2 = Math.toRadians(latitude2);
+        // 经度
+        double lng1 = Math.toRadians(longitude1);
+        double lng2 = Math.toRadians(longitude2);
+        // 纬度之差
+        double a = lat1 - lat2;
+        // 经度之差
+        double b = lng1 - lng2;
+        // 计算两点距离的公式
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+                Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(b / 2), 2)));
+        // 弧长乘地球半径, 返回单位: 千米
+        s = s * EARTH_RADIUS * 1000;
+       
+        return s;
+    }
 }
